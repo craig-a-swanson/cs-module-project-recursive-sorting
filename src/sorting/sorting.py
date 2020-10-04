4d1ab9d9afbf1d88374ade95c8bb019258dff4d9
Craig Swanson <cswanson02@me.com>
2020-10-04 09:42:15 -0500

    Finished merge_sort; project MVP

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -3,42 +3,52 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
 
+    # keep track of indexes for three separate arrays
     indexA = 0
     indexB = 0
-    for index in range(0, len(merged_arr)):
-        if arrA is not None:
-            if arrA[indexA] <= arrB[indexB]:
-                merged_arr[index] = arrA[indexA]
-                if indexA < (len(arrA) - 1):
-                    indexA += 1
-                    print(indexA)
-                else:
-                    arrA = None
-            else:
-                merged_arr[index] = arrB[indexB]
-                indexB += 1
-                print(indexB)
-        elif arrB is not None:
-            merged_arr[index] = arrB[indexB]
-            if indexB < (len(arrB) - 1):
-                indexB += 1
-                print(f'indexB after A is done: {indexB}')
-            else:
-                arrB = None
+    index_merged = 0
 
+    # the arrays being compared could be different lengths and
+    # one will reach the end before the other; while neither is
+    # at the end, compare the two values, insert the lower value
+    # into the merged_arr, and increment the indexes for those two.
+    while indexA < len(arrA) and indexB < len(arrB):
+        if arrA[indexA] <= arrB[indexB]:
+            merged_arr[index_merged] = arrA[indexA]
+            index_merged += 1
+            indexA += 1
+        else:
+            merged_arr[index_merged] = arrB[indexB]
+            index_merged += 1
+            indexB += 1
+    
+    # if arrB finished first, go throught the rest of arrA
+    # and insert its values into merged_arr
+    while indexA < len(arrA):
+        merged_arr[index_merged] = arrA[indexA]
+        index_merged += 1
+        indexA += 1
 
+    # if arrA finished first, go throught the rest of arrB
+    # and insert its values into merged_arr
+    while indexB < len(arrB):
+        merged_arr[index_merged] = arrB[indexB]
+        index_merged += 1
+        indexB += 1
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # split array in half until the length equals one
-    if len(arr) == 1:
-        return arr
-    elif len(arr) > 1:
-        return merge_sort(arr[0:((len(arr) - 1) // 2)])
-        return merge_sort(arr[((len(arr) - 1) + 1):])
-
+    # print(arr)
+    if len(arr) > 1:
+        left_array = arr[:((len(arr)) // 2)]
+        right_array = arr[((len(arr)) // 2):]
+        left_array = merge_sort(left_array)
+        right_array = merge_sort(right_array)
+        arr = merge(left_array, right_array)
+        # print(f'in merge_sort before return: {arr}')
 
     return arr
 
@@ -58,4 +68,4 @@ arr1 = [1, 5, 8, 2, 9, 6, 0, 3, 7]
 print(merge_sort(arr1))
 
 arr2 = [4, 10, 13, 12]
-print(merge(arr1, arr2))
+# print(merge(arr1, arr2))
